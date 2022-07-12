ca2e2a9defec5933d21b6b4c9bfc06e769eb900e
chano64123 <promo201674@gmail.com>
2022-07-12 16:02:31 -0500

    fix module cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 const loguer = require('./loguerMiddleware')
-const cors = require('./cors')
+
+const app = express()
 
 app.use(cors())
 app.use(express.json())
